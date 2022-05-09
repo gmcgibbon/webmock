bc13509f0e2ae98b7faf4d3a46521f052e77a730
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2022-05-09 19:11:19 -0400

    Autoload

--- lib/webmock.rb
+++ lib/webmock.rb
@@ -4,56 +4,52 @@ require 'addressable/uri'
 require 'addressable/template'
 require 'crack/xml'
 
-require_relative 'webmock/deprecation'
-require_relative 'webmock/version'
+require 'webmock/deprecation'
+require 'webmock/version'
+require 'webmock/errors'
 
-require_relative 'webmock/errors'
+module WebMock
+  module Util
+    autoload(:QueryMapper, 'webmock/util/query_mapper')
+    autoload(:URI, 'webmock/util/uri')
+    autoload(:Headers, 'webmock/util/headers')
+    autoload(:HashCounter, 'webmock/util/hash_counter')
+    autoload(:HashKeysStringifier, 'webmock/util/hash_keys_stringifier')
+    autoload(:ValuesStringifier, 'webmock/util/values_stringifier')
+    autoload(:JSON, 'webmock/util/json')
+  end
+  autoload(:VersionChecker, 'webmock/util/version_checker')
+  autoload(:HashValidator, 'webmock/util/hash_validator')
 
-require_relative 'webmock/util/query_mapper'
-require_relative 'webmock/util/uri'
-require_relative 'webmock/util/headers'
-require_relative 'webmock/util/hash_counter'
-require_relative 'webmock/util/hash_keys_stringifier'
-require_relative 'webmock/util/values_stringifier'
-require_relative 'webmock/util/json'
-require_relative 'webmock/util/version_checker'
-require_relative 'webmock/util/hash_validator'
+  module Matchers
+    autoload(:HashArgumentMatcher, 'webmock/matchers/hash_argument_matcher')
+    autoload(:HashExcludingMatcher, 'webmock/matchers/hash_excluding_matcher')
+    autoload(:HashIncludingMatcher, 'webmock/matchers/hash_including_matcher')
+    autoload(:AnyArgMatcher, 'webmock/matchers/any_arg_matcher')
+  end
 
-require_relative 'webmock/matchers/hash_argument_matcher'
-require_relative 'webmock/matchers/hash_excluding_matcher'
-require_relative 'webmock/matchers/hash_including_matcher'
-require_relative 'webmock/matchers/any_arg_matcher'
+  autoload(:RequestPattern, 'webmock/request_pattern')
+  autoload(:RequestSignature, 'webmock/request_signature')
+  autoload(:ResponsesSequence, 'webmock/responses_sequence')
+  autoload(:RequestStub, 'webmock/request_stub')
+  autoload(:Response, 'webmock/response')
+  autoload(:RackResponse, 'webmock/rack_response')
 
-require_relative 'webmock/request_pattern'
-require_relative 'webmock/request_signature'
-require_relative 'webmock/responses_sequence'
-require_relative 'webmock/request_stub'
-require_relative 'webmock/response'
-require_relative 'webmock/rack_response'
+  autoload(:StubRequestSnippet, 'webmock/stub_request_snippet')
+  autoload(:RequestSignatureSnippet, 'webmock/request_signature_snippet')
+  autoload(:RequestBodyDiff, 'webmock/request_body_diff')
 
-require_relative 'webmock/stub_request_snippet'
-require_relative 'webmock/request_signature_snippet'
-require_relative 'webmock/request_body_diff'
+  autoload(:AssertionFailure, 'webmock/assertion_failure')
+  autoload(:RequestExecutionVerifier, 'webmock/request_execution_verifier')
+  autoload(:Config, 'webmock/config')
+  autoload(:CallbackRegistry, 'webmock/callback_registry')
+  autoload(:RequestRegistry, 'webmock/request_registry')
+  autoload(:StubRegistry, 'webmock/stub_registry')
 
-require_relative 'webmock/assertion_failure'
-require_relative 'webmock/request_execution_verifier'
-require_relative 'webmock/config'
-require_relative 'webmock/callback_registry'
-require_relative 'webmock/request_registry'
-require_relative 'webmock/stub_registry'
-require_relative 'webmock/api'
+  autoload(:HttpLibAdapters, 'webmock/http_lib_adapters')
+  autoload(:HttpLibAdapterRegistry, 'webmock/http_lib_adapters/http_lib_adapter_registry')
+  autoload(:HttpLibAdapter, 'webmock/http_lib_adapters/http_lib_adapter')
 
-require_relative 'webmock/http_lib_adapters/http_lib_adapter_registry'
-require_relative 'webmock/http_lib_adapters/http_lib_adapter'
-require_relative 'webmock/http_lib_adapters/net_http'
-require_relative 'webmock/http_lib_adapters/http_rb_adapter'
-require_relative 'webmock/http_lib_adapters/httpclient_adapter'
-require_relative 'webmock/http_lib_adapters/patron_adapter'
-require_relative 'webmock/http_lib_adapters/curb_adapter'
-require_relative 'webmock/http_lib_adapters/em_http_request_adapter'
-require_relative 'webmock/http_lib_adapters/typhoeus_hydra_adapter'
-require_relative 'webmock/http_lib_adapters/excon_adapter'
-require_relative 'webmock/http_lib_adapters/manticore_adapter'
-require_relative 'webmock/http_lib_adapters/async_http_client_adapter'
-
-require_relative 'webmock/webmock'
+  require 'webmock/api'
+  require 'webmock/webmock'
+end
